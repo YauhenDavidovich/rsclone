e78b04224ae3d9aeb8eab2fecbe04b83ec8ba219
Yauhen Davidovich <davidovich336@gmail.com>
2021-01-12 23:38:21 +0300

    feat: add background scrolling

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,11 +1,15 @@
 import Phaser from 'phaser'
 import SceneKeys from "~/consts/SceneKeys";
 import AnimationKeys from "~/consts/AnimationKeys";
+import TextureKeys from "~/consts/TextureKeys";
 
 export default class Game extends Phaser.Scene {
     constructor() {
         super(SceneKeys.Game)
     }
+
+    private background!: Phaser.GameObjects.TileSprite
+
 
     create() {
 
@@ -13,12 +17,36 @@ export default class Game extends Phaser.Scene {
         const width = this.scale.width
         const height = this.scale.height
 
-        this.add.image(0, height,  'background').setOrigin(0, 1)
-            .setScrollFactor(0.5)
-        this.add.sprite(width * 0.5, height * 0.5, 'hero', 'run_1.png').play(AnimationKeys.RunningManRun)
-            .setScrollFactor(0.5)
-        this.cameras.main.setBounds(0, 0, width * 3, height)
+        this.background = this.add.tileSprite(
+            0, 0,
+            width, height,
+            TextureKeys.Background
+        )
+            .setOrigin(0, 0)
+            .setScrollFactor(0, 0)
+
+
+
+
+
+        const hero = this.physics.add.sprite(width * 0.5, height * 0.5, 'hero', 'run_1.png').play(AnimationKeys.RunningManRun)
+
+        const body = hero.body as Phaser.Physics.Arcade.Body
+        body.setCollideWorldBounds(true)
+        // set the x velocity to 200
+        body.setVelocityX(200)
+
+        this.physics.world.setBounds(
+            0, 0, // x, y
+            Number.MAX_SAFE_INTEGER, height - 30 // width, height
+        )
+
+        this.cameras.main.startFollow(hero)
+        this.cameras.main.setBounds(0, 0, Number.MAX_SAFE_INTEGER, height)
 
     }
-
+    update(t: number dt: number)
+     {
+         this.background.setTilePosition(this.cameras.main.scrollX)
+     }
 }
